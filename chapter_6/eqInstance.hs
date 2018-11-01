e0d7a17fb36833c3984e891e5065161d4eac7dbe
ashab <asha.ab@gmail.com>
2018-10-31 22:36:44 -0400

    Eq instance for TwoIntegers

--- chapter_6/eqInstance.hs
+++ chapter_6/eqInstance.hs
@@ -4,4 +4,13 @@ data TisAnInteger =
 instance Eq TisAnInteger where
   (TisAn a) == (TisAn a') = a == a'
 
+data TwoIntegers =
+  Two Integer Integer
+
+instance Eq TwoIntegers where
+  (Two a b) == (Two a' b') = a == a' && b == b'
+
+data StringOrInt =
+    TisAnInt   Int
+  | TisAString String
 
