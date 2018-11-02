a3cb137fdf264bb921bc268361a31098f727fce0
ashab <asha.ab@gmail.com>
2018-11-01 22:57:19 -0400

    Unfinished eq instances

--- chapter_6/eqInstance.hs
+++ chapter_6/eqInstance.hs
@@ -14,3 +14,21 @@ data StringOrInt =
     TisAnInt   Int
   | TisAString String
 
+data Pair a =
+  Pair a a
+
+instance Eq a => Eq (Pair a) where
+  (Pair a1 a2) == (Pair a1' a2') = a1 == a1' && a2 == a2'
+
+data Tuple a b =
+  Tuple a b
+
+instance (Eq a, Eq b) => Eq (Tuple a b) where
+  (Tuple a b) == (Tuple a' b') = a == a' && b == b'
+
+data Which a =
+    ThisOne a
+  | ThatOne a
+
+instance Eq a => Eq (Which a) where
+  (ThisOne a | ThatOne b) == (ThisOne a' | ThatOne b') = (a == a') || (b == b')
