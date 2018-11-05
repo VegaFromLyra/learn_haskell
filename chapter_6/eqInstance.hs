0cfff11537d39ad0ab40b965f15521def7a60fe0
ashab <asha.ab@gmail.com>
2018-11-04 22:40:58 -0500

    Remainder of the Eq instances

--- chapter_6/eqInstance.hs
+++ chapter_6/eqInstance.hs
@@ -11,8 +11,12 @@ instance Eq TwoIntegers where
   (Two a b) == (Two a' b') = a == a' && b == b'
 
 data StringOrInt =
-    TisAnInt   Int
-  | TisAString String
+  TisAnInt Int | TisAString String
+
+instance Eq StringOrInt where
+  (==) (TisAnInt a) (TisAnInt a') = a == a'
+  (==) (TisAString b) (TisAString b') = b == b'
+  (==) _ _ = False
 
 data Pair a =
   Pair a a
@@ -26,9 +30,18 @@ data Tuple a b =
 instance (Eq a, Eq b) => Eq (Tuple a b) where
   (Tuple a b) == (Tuple a' b') = a == a' && b == b'
 
-data Which a =
-    ThisOne a
-  | ThatOne a
+data Which a
+  = ThisOne a | ThatOne a
 
 instance Eq a => Eq (Which a) where
-  (ThisOne a | ThatOne b) == (ThisOne a' | ThatOne b') = (a == a') || (b == b')
+  ThisOne a == ThisOne a' = a == a'
+  ThatOne a == ThatOne a' = a == a'
+  _ == _ = False
+
+data EitherOr a b =
+  Hello a | Goodbye b
+
+instance (Eq a, Eq b) => Eq (EitherOr a b) where
+  Hello a == Hello a' = a == a'
+  Goodbye b == Goodbye b' = b == b'
+  _ == _ = False
